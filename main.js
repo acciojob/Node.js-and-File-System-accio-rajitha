56350cec0843c9ce95c318578c8dfd6b8ca3446b
accio-rajitha <rajithasabhavath2019@gmail.com>
2024-06-17 16:00:59 +0530

    code added

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const readline = require('readline');
+const path = require('path');
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -8,7 +9,26 @@ const rl = readline.createInterface({
 
 rl.question('Enter the filename: ', (filename) => {
   rl.question('Enter the word to remove: ', (word) => {
-    // TODO: Implement this function
-    rl.close();
+    const filePath = path.resolve(filename); 
+
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        console.error(`Error reading the file ${filePath}:`, err);
+        rl.close();
+        return;
+      }
+
+      
+      const modifiedContent = data.replace(new RegExp('\\b' + word + '\\b', 'gi'), '');
+
+      fs.writeFile(filePath, modifiedContent, 'utf8', (err) => {
+        if (err) {
+          console.error(`Error writing to the file ${filePath}:`, err);
+        } else {
+          console.log(`Successfully removed all occurrences of '${word}' from ${filePath}.`);
+        }
+        rl.close();
+      });
+    });
   });
 });
